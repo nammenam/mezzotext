98b95f0b763d6d22e917a72ecca350db58c43047
Brage Wiseth <brage.wis@gmail.com>
2026-06-06 23:22:55 +0200

    update

---
+++ src/root.zig
@@ -0,0 +1,10 @@
+const std = @import("std");
+const Io = std.Io;
+
+pub fn add(a: i32, b: i32) i32 {
+    return a + b;
+}
+
+test "basic add functionality" {
+    try std.testing.expect(add(3, 7) == 10);
+}
